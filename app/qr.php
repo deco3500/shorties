990cf41d9a5983ca340796511c1003238898b303
sivi choun <aiiish.sc@gmail.com>
2016-10-14 10:48:40 +1000

    camera function working

--- app/qr.php
+++ app/qr.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -7,45 +7,37 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="img/logo.png">
-	<title>Notice Me!</title>
-	<!-- ReactJS -->
-	<script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
-	<script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
-	<!-- jQuery -->
+    <title>Notice Me!</title>
+     ReactJS -
+    <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
+    <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
+    <!-- jQuery -
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <!-- Bootstrap -->
+    <!-- Bootstrap -
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- index-->
+    <!-- index
     <link href="css/index.css" rel="stylesheet">
     <script src="js/main.js"></script>
-    <!-- main-->
+    <!-- main
     <link href="css/main.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <!-- News-->
+    <!-- News
     <link href="css/news.css" rel="stylesheet">
   </head>
   
-  <body>
-	  <header class="header">
-		<img src="img/back.png" width="40" height="40" alt="Back">
-	    <h1 class="header__title">Notice Me!</h1>
-	  </header>
-	  
-	  <main>
-		  <div class="extraInfo">
-		  	<h3 id="get_location" align="center"></h3>
-		  </div>
-		  <div id="loadPage">
-		  </div>
-    </main>
+  <body> 
+      <header class="header"></header>
+        <img src="img/back.png" width="40" height="40" alt="Back">
+        <h1 class="header__title">Notice Me!</h1>-->
+      <div class="load_continer">
+      
 
-        <main>
         <video id="video" width="640" height="480" autoplay></video>
-        <a href="main.php" clss=btn id="snap">Snap Photo</a>
+        <a href="#" clss=btn id="snap">Snap Photo</a>
         <canvas id="canvas" width="640" height="480"></canvas>
-    </main>
+    </div>
 
 <script type="text/javascript">
     // Grab elements, create settings, etc.
@@ -90,6 +82,6 @@ document.getElementById("snap").addEventListener("click", function() {
 });
 </script>
 
-  </body>
-</html>
+ <!-- </body>
+</html>-->
 
